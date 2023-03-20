4b99da4adc95e01e492f8600997b33abb4db6bc3
Wanderson <wandersonsousa3004@gmail.com>
2023-03-20 11:59:03 -0300

    feat: update text

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -11,7 +11,7 @@ import { SITE_NAME } from '@/utils/constants'
 
 const HomePage = () => {
   const { authenticated } = useUser()
-  useEffect(() => {
+  /* useEffect(() => {
     window.tolstoyAppKey = 'b715dae7-c1b6-4118-bc93-112b0c567563'
     const s = document.createElement('script')
     s.type = 'text/javascript'
@@ -22,15 +22,17 @@ const HomePage = () => {
     return () => {
       document.head.removeChild(s)
     }
-  }, [])
+  }, []) */
 
   return (
-    <>
+    <Box style={{
+      height: '100vh',
+    }}>
       <Header authenticated={Boolean(authenticated)} />
       <Box
         sx={{
           width: '100vw',
-          height: '100vh',
+          height: '100%',
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
@@ -38,53 +40,54 @@ const HomePage = () => {
           backgroundColor: 'white'
         }}
       >
-        <Box>
-          <Grid
-            container
-            sx={{
-              width: '100vw',
-              backgroundImage: 'url(/images/app/site_banner.jpg)',
-              backgroundRepeat: 'no-repeat',
-              backgroundPosition: 'center',
-              backgroundSize: 'cover',
-              minHeight: '100vh',
-              mx: 'auto',
-              pt: 12
-            }}
-            alignItems={'center'}
-          >
-            <Grid container item xs={12} md={6}></Grid>
+        <Grid
+          container
+          sx={{
+            width: '100vw',
 
-            <Grid item xs={12} md={6} sx={{ px: 2 }}>
-              <Typography variant='h3' sx={{ mb: 10, lineHeight: 1 }}>
-                LISTA-IG A MAIOR EM EXTRAÇÃO DE CONTATOS
+            backgroundImage: 'url(/images/app/site_banner.jpg)',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            minHeight: `100%`,
+            mx: 'auto',
+            pt: 12,
+          }}
+          alignItems={'center'}
+        >
+          <Grid container item xs={12} md={6}></Grid>
+
+          <Grid item xs={12} md={6} sx={{ px: 2 }}>
+            <Typography variant='h3' sx={{ mb: 10, lineHeight: 1, color: 'white' }}>
+              <span style={{
+                textTransform: 'uppercase',
+              }}>{SITE_NAME}</span>: A MAIOR EM EXTRAÇÃO DE CONTATOS
+            </Typography>
+            <Typography variant='body1' sx={{ lineHeight: 1.2, mt: 5, color: '#ecf0f1' }}>
+              Olá! Seja bem vido a {SITE_NAME} a maior plataforma de extração de ‘leads’ no Brasil aqui você vai
+              extrair por Idade, Sexo M/F, Renda Salarial, Cidade  e Estado os contatos vem com nomes de usuários dos
+              números.
+            </Typography>
+
+            <Box sx={{ mt: 5, }}>
+              <Typography component='p' variant='caption' style={{ color: '#ecf0f1', opacity: 0.9 }}>
+                11982969676
               </Typography>
-              <Typography variant='body1' sx={{ lineHeight: 1.2, mt: 5 }}>
-                Olá! Seja bem vido a {SITE_NAME} a maior plataforma de extração de ‘leads’ no Brasil aqui você vai
-                extrair por Idade, Sexo M/F, Renda Salarial, Cidade  e Estado os contatos vem com nomes de usuários dos
-                números.
+              <Typography component='p' variant='caption' style={{ color: '#ecf0f1', opacity: 0.9 }}>
+                financeiro@safez.com.br
               </Typography>
+              <Typography component='p' variant='caption' style={{ color: '#ecf0f1', opacity: 0.9 }}>
+                Rua Placeholder - ST
+              </Typography>
+            </Box>
 
-              <Box sx={{ mt: 5 }}>
-                <Typography component='p' variant='caption'>
-                  11982969676
-                </Typography>
-                <Typography component='p' variant='caption'>
-                  financeiro@listaig.com.br
-                </Typography>
-                <Typography component='p' variant='caption'>
-                  Rua Estados Unidos Jardim América - SP
-                </Typography>
-              </Box>
-
-              <Link passHref href='/register'>
-                <Button variant='contained' sx={{ mt: 6 }}>
-                  Quero extrair meus leads agora
-                </Button>
-              </Link>
-            </Grid>
+            <Link passHref href='/register'>
+              <Button variant='contained' sx={{ mt: 6 }}>
+                Quero extrair meus leads agora
+              </Button>
+            </Link>
           </Grid>
-        </Box>
+        </Grid>
         {/*  <SpeedDial
           ariaLabel='SpeedDial basic example'
           sx={{ position: 'absolute', bottom: 16, right: 16 }}
@@ -92,7 +95,7 @@ const HomePage = () => {
         ></SpeedDial> */}
       </Box>
       <Footer />
-    </>
+    </Box>
   )
 }
 
